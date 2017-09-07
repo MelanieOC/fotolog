ed64e77eb977e66c326c2d6e0e2c00d5d5024916
MelanieOC <melanie.ocharan@gmail.com>
2017-09-07 15:07:18 -0500

    avanzando

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 /*aquí va tu código*/
+$("#guardar").on('click', guardarDatos);
+$("#limpiar").on('click', limpiar);
+console.log(localStorage);
+function guardarDatos() {
+	localStorage.setItem($("#clave").val(),$("#valor").val())
+	
+	console.log(localStorage);
+}
+
+function recuperarDatos() {
+	if ((localStorage.nombre != undefined) && (localStorage.password != undefined)) {
+		$("#datos").html("Nombre: " + localStorage.nombre + "<br> Password: " + localStorage.password);
+	}else{
+		$("#datos").html('No has introducido tu nombre y tu password');
+	}
+}
+
+function limpiar() {
+	localStorage.clear();
+}
