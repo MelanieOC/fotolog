691b73d1fe29967beff872a9f769a71d7cdac9c1
MelanieOC <melanie.ocharan@gmail.com>
2017-09-08 06:24:31 -0500

    funciona, falta css

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
 /*aquí va tu código*/
 $("#guardar").on('click', guardarDatos);
 $("#limpiar").on('click', limpiar);
-console.log(localStorage);
-function guardarDatos() {
-	localStorage.setItem($("#clave").val(),$("#valor").val())
-	
-	console.log(localStorage);
-}
 
-function recuperarDatos() {
-	if ((localStorage.nombre != undefined) && (localStorage.password != undefined)) {
-		$("#datos").html("Nombre: " + localStorage.nombre + "<br> Password: " + localStorage.password);
-	}else{
-		$("#datos").html('No has introducido tu nombre y tu password');
+function guardarDatos() {
+	if($("#clave").val() && $("#valor").val()){
+		localStorage.setItem(localStorage.length, $("#clave").val() +','+ $("#valor").val());
+		$("#clave").val('');
+		$("#valor").val('');
+		comentario();
 	}
 }
 
+function comentario() {
+	$("#ale").empty();
+	if (localStorage.length > 0) {
+		$.each(localStorage, (key, value)=>{
+			var array = value.split(',');
+			$("<div>").addClass('comentario row').append(
+				$('<div>').addClass('col-md-2 col-sm-2 col-xs-2').append(
+					$("<i>").addClass('fa fa-user-circle fa-5x fa-fw')
+				)
+			).append(
+				$('<div>').addClass('col-md-10 col-sm-10 col-xs-10').append(
+					$("<h3>").html(array[0])
+				).append(
+				$("<p>").html(array[1])
+			)).appendTo("#ale");
+		})
+	}
+}
+
+comentario();
+
 function limpiar() {
 	localStorage.clear();
+	comentario();
 }
